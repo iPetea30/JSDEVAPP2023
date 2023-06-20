b2ea15f34ae5f5c3d2485a1a93974e6fee0a0d3b
iPetea30 <dumi.puia@gmail.com>
2023-06-20 21:00:20 +0300

    reactBlog

---
+++ reactBlog/backend/types/Post.ts
@@ -0,0 +1,12 @@
+export interface BasicPost {
+    id: number,
+  }
+
+export interface Post extends BasicPost {
+    titlu: string,
+    continut: string,
+    categorie_id?: number,
+    dataadaugare?: Date,
+    user_id?: number,
+    poza?:string,
+}
